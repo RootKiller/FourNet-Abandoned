56078d78015a4462acd74d017739df981575ce2d
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2016-07-11 18:42:08 +0200

    - Launcher: No longer use Steam in launcher.

--- src/Client/Launcher/Main.cpp
+++ src/Client/Launcher/Main.cpp
@@ -6,7 +6,6 @@
 #include "Strings.h"
 #include "OS/OS.h"
 
-#include "Steam/SteamWrapper.h"
 #include "Memory/Hooking/Injecting.h"
 
 namespace launcher {
@@ -23,24 +22,11 @@ extern const char* GAME_EXE_NAME;
  */
 int AppMain(int argc, char *argv[])
 {
-	SteamWrapper steam;
-	if (!steam.Init()) {
-		return 0;
-	}
+	// Patryk do: Get this path from registry (HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Rockstar Games\Grand Theft Auto IV)
+	// (WOW6432Node will likely not be there on 32bit os - better handle it explicitly so we will not put our hands into
+	// cup of shit when microsoft changes something in registry handling)
 
-	if (!steam.IsGameInstalled()) {
-		AString128 errorMessage;
-		errorMessage.Format("To run you need to have installed %s game.", GAME_NAME);
-		OS::ShowMessageBox("Fatal Error", errorMessage, OS::EMessageBoxType::MESSAGE_BOX_TYPE_error);
-		return 0;
-	}
-
-	PathString gamePath;
-	steam.GetGameInstallPath(gamePath);
-	if (gamePath.IsEmpty()) {
-		OS::ShowMessageBox("Fatal Error", "Unable to find game installation path.", OS::EMessageBoxType::MESSAGE_BOX_TYPE_error);
-		return 0;
-	}
+	const PathString gamePath("E:\\Program Files (x86)\\Steam\\steamapps\\common\\Grand Theft Auto IV");
 
 	PathString gameExePath;
 	gameExePath.Format("%s\\%s%s", *gamePath, GAME_EXE_RELATIVE_PATH, GAME_EXE_NAME);
@@ -49,14 +35,13 @@ int AppMain(int argc, char *argv[])
 	PROCESS_INFORMATION processInformation = { 0 };
 	startupInfo.cb = sizeof(startupInfo);
 
-	SetEnvironmentVariableA("SteamAppID", STRINGIZE(GAME_APPID));
-
 	if (GetFileAttributesA(gameExePath) == INVALID_FILE_ATTRIBUTES) {
 		OS::ShowMessageBox("Fatal Error", "Unable to find game .exe file.\n\nGame file: " + gameExePath, OS::EMessageBoxType::MESSAGE_BOX_TYPE_error);
 		return 0;
 	}
 
-	if (!CreateProcessA(gameExePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, gamePath, &startupInfo, &processInformation)) {
+	const LPSTR cmdLine = GetCommandLine();
+	if (!CreateProcessA(gameExePath, cmdLine, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, gamePath, &startupInfo, &processInformation)) {
 		const unsigned lastError = GetLastError();
 		AString1024 errorMessage;
 		errorMessage.Format("Cannot create game process.\n\nGame file: %s\nDirectory: %s\n\n(Error code: %u)", *gameExePath, *gamePath, lastError);
