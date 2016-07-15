ed34295db41ba1a9548d44dd0a9a0fe486fffdf7
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2016-07-15 19:00:25 +0200

    - GTAIVLib: Implementation of ped health setting and getting.

--- src/Client/Game/GTAIVLib/Entities/Peds/Ped.cpp
+++ src/Client/Game/GTAIVLib/Entities/Peds/Ped.cpp
@@ -9,4 +9,20 @@ bool _declspec(naked) CPed::SetCharCreatedBy(const EPedCreator a2)
 	_asm jmp Offsets::CPed::SetCharCreatedBy;
 }
 
+void CPed::SetHealth(const float health)
+{
+	// As long as we do not use XLive we do not
+	// need to worry about that this value will be
+	// obfuscated somehow so just operate on it normally.
+	*reinterpret_cast<float*>(m_health->data) = health;
+}
+
+float CPed::GetHealth(void) const
+{
+	// As long as we do not use XLive we do not
+	// need to worry about that this value will be
+	// obfuscated somehow so just operate on it normally.
+	return *reinterpret_cast<float*>(m_health->data);
+}
+
 /* eof */
--- src/Client/Game/GTAIVLib/Entities/Peds/Ped.h
+++ src/Client/Game/GTAIVLib/Entities/Peds/Ped.h
@@ -11,6 +11,12 @@ enum EPedCreator
 	PED_CREATOR_SCRIPT = 2,
 };
 
+struct XLiveProtectedBuffer
+{
+	uint8 pad_XLiveProtectedBuffer0[4 + 4 + 4 * 2];
+	uint8 data[4];
+};
+
 class CPed : public CPhysical
 {
 public:
@@ -19,9 +25,17 @@ public:
 	uint8							pad552[56];			// 0552 - 0608
 	uint32							m_unknownFlags608;	// 0608 - 0612
 	uint32							m_unknownFlags612;	// 0612 - 0616
-	uint8							pad616[3224];		// 0616 - 3840
+	uint8							pad616[3140];		// 0616 - 3756
+	XLiveProtectedBuffer			*m_health;			// 3756 - 3760
+	uint8							pad3760[80];		// 3760 - 3840
 
 	bool		SetCharCreatedBy	(const EPedCreator creator);
+
+	/** Sets health of ped */
+	void		SetHealth			(const float health);
+
+	/** Gets health of ped */
+	float		GetHealth			(void) const;
 };
 static_assert(sizeof(CPed) == 3840, "Incorrect CPed size");
 
